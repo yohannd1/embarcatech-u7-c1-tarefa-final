de82bc845589c34bdfbc62abff47f415470091b1
yohannd1 <mitonanan12@gmail.com>
2025-02-22 11:54:56 -0300

    código para cálculo de ângulo do joystick

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
@@ -24,7 +28,17 @@ int main(void) {
 	while (true) {
 		uint16_t x_axis = adc_wrapper_read(joystick_x);
 		uint16_t y_axis = adc_wrapper_read(joystick_y);
-		printf("X=%u, Y=%u\n", x_axis, y_axis);
+
+		float centered[2] = { (float)x_axis - 2048.0f, (float)y_axis - 2048.0f };
+		float normalized[2] = { centered[0] / 2048.0f, centered[1] / 2048.0f };
+		float mag = sqrtf(normalized[0] * normalized[0] + normalized[1] * normalized[1]);
+
+		if (mag > 0.45f) {
+			float angle = atan2f(normalized[1], normalized[0]);
+			if (angle < 0.0f)
+				angle += 2 * M_PI;
+			printf("(%.8f %.8f) ~ %.8f, ang %.8f pi rad\n", normalized[0], normalized[1], mag, angle / M_PI);
+		}
 
 		sleep_ms(500);
 	}
